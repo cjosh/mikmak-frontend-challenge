87f144df8d6d83909307e27301e91b5a03899a38
josh smith <c.joshsmith@gmail.com>
2017-12-04 05:14:05 -0500

    Mikmak Vue Challenge

---
+++ vue/template.js
@@ -0,0 +1,62 @@
+const images = [
+    {url: 'https://images.pexels.com/photos/627928/pexels-photo-627928.png?w=1260&h=750&auto=compress&cs=tinysrgb'},
+    {url: 'https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb'},
+    {url: 'https://images.pexels.com/photos/326235/pexels-photo-326235.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb'},
+    {url: 'https://images.pexels.com/photos/414075/pexels-photo-414075.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb'}    
+]
+
+const Slide = {
+    props: ['image', 'index'],
+    template: `<img v-bind:src="image.url"/>`
+}
+
+Vue.component('carousel', {
+    data() {
+        return {
+            title: 'Vue.js Carousel',
+            images: images,
+            active: 0 
+        }
+    },
+    components: {
+        'slide': Slide
+    },
+    methods: {
+        goToNextSlide() {
+            this.active++;
+            if (this.active > this.images.length - 1) {
+                this.active = 0;
+            }
+        },
+        goToPreviousSlide() {
+            this.active--; 
+            if (this.active < 0) {
+                this.active = this.images.length - 1;
+            }    
+        }
+    },
+    template: `<div>
+                <h1 class="app-title">{{ title }}</h1>
+                <div v-if="!images">
+                    Please add images to use the carousel
+                </div>
+                <div v-else class="carousel">
+                    <div class="images">
+                        <slide
+                            v-for="(image, key) in images" 
+                            v-bind:image="image" 
+                            v-bind:index="key"
+                            v-bind:class="{ active: key === active }">
+                        </slide>
+                    </div>
+                    <button v-on:click="goToPreviousSlide" class="button-prev"> [<] </button>
+                    <button v-on:click="goToNextSlide" class="button-next"> [>] </button>                    
+                </div>
+            </div>`,
+            
+    })
+
+new Vue({
+    el: '#app'
+  })
+
